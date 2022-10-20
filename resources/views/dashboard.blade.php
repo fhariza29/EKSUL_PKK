e26dea4604bffce84c5b9ad2b1a13eec5652dd32
fhariza29 <fharizaalghifera16834@gmail.com>
2022-10-20 15:53:30 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    {{-- My css --}}
+    <link rel="stylesheet" href="{{ asset('assets/style.css') }}">
+    <title>LANDING PAGE</title>
+</head>
+<body>
+    <div class="navbar">
+        <div class="right">
+            LOGO
+        </div>
+        <div class="left">
+            <tr>
+                <a href="">lorem</a>
+            </tr>
+            <tr>
+                <a href="">lorem</a>
+            </tr>
+            <tr>
+                <a href="">lorem</a>
+            </tr>
+            <tr>
+                <a href="">lorem</a>
+            </tr>
+            <tr>
+                <a href="">lorem</a>
+            </tr>
+        </div>
+    </div>
+    <!-- Hero Banner Section -->
+<section class="hero-banner bg-light py-5">
+    <div class="container">
+        <div class="row row align-items-center">
+            <div class="col-lg-5 offset-lg-1 order-lg-1">
+                <img src="{{asset('assets/img/pattern.png')}}" class="img-fluid" alt="Web Development">
+            </div>
+            <div class="col-lg-6">
+            <h2 class="well" style="font: bold;" >Ekstrakulikuler</h2> 
+                   <h2 class="well">SMKN 1 Sumedang</h2>
+                  <h6>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</h6>
+            </div>
+        </div>
+        <div class="lorem">
+            <div class="konten">
+                <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries por previewing layouts and visual mockups</p>
+            </div>
+        </div>
+            <div class="garis"></div>
+        <div class="loremdua">
+            <div class="left">
+                <div class="kontensatu">
+                    <h5>Lorem</h5>
+                </div>
+                <div class="kontendua">
+                    <h2>Lorem Ipsum Dolor Sit Amet</h2>
+                </div>
+            </div>
+            <div class="right">
+                <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+        </div>
+    </div>
+</section>
+    </div>
+    </div>
+</body>
+</html>
